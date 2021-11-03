13aa0ea579d87cf7c1c0ff22c98780830562a980
AkshayBenny <akshaybennygh@gmail.com>
2021-11-03 23:55:27 +0530

    update

--- portfolio/src/components/home/Home.js
+++ portfolio/src/components/home/Home.js
@@ -17,12 +17,28 @@ function Home() {
         <h2>Creative Works</h2>
       </div>
       <div className="creative-cards-cont-row-one">
-        <Creative />
-        <Creative />
+        <Creative
+          img="https://images.unsplash.com/photo-1635898445837-65c066642d81?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
+          name="Deep in the desert"
+          subname="Freelance Project"
+        />
+        <Creative
+          img="https://images.unsplash.com/photo-1635910162005-4a295b1bcca6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
+          name="Deep in the desert"
+          subname="Freelance Project"
+        />
       </div>
       <div className="creative-cards-cont-row-two">
-        <Creative />
-        <Creative />
+        <Creative
+          img="https://images.unsplash.com/photo-1635915575783-2cc97830a1a1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=715&q=80"
+          name="Deep in the desert"
+          subname="Freelance Project"
+        />
+        <Creative
+          img="https://images.unsplash.com/photo-1635899256324-4d14da5dd8b6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=765&q=80"
+          name="Deep in the desert"
+          subname="Freelance Project"
+        />
       </div>
     </div>
   );
--- portfolio/src/components/home/home_components/creative/Creative.js
+++ portfolio/src/components/home/home_components/creative/Creative.js
@@ -4,13 +4,10 @@ import "./Creative.css";
 function Creative(props) {
   return (
     <div className="creative-cont">
-      <img
-        src="https://images.unsplash.com/photo-1635898445837-65c066642d81?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
-        alt="mountains"
-      />
+      <img src={props.img} alt="mountains" />
       <div className="details-cont">
-        <p className="cheading">Deep in the jungle</p>
-        <p className="csubheading">Freelance Project</p>
+        <p className="cheading">{props.name}</p>
+        <p className="csubheading">{props.subname}</p>
         <p className="clink">Read More</p>
       </div>
     </div>
