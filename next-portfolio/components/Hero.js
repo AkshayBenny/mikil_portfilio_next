18785bba50089abf812792e31fbfef3c4e12f740
AkshayBenny <akshaybennygh@gmail.com>
2022-02-16 22:04:00 +0530

    update

--- next-portfolio/components/Hero.js
+++ next-portfolio/components/Hero.js
@@ -1,9 +1,12 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 
 function Hero() {
   return (
     <div className='h-screen  bg-orange-300'>
-      This is the hero section
+      <h1>Hello World!</h1>
+      <h2>I'm Akshay Benny</h2>
+      <h3>Web Developer</h3>
     </div>
   );
 }
