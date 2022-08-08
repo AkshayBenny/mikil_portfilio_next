c504aec2d0926240f646c00d331b3f0ffb73f39f
Akshay Benny <akshaybennygh@gmail.com>
2022-08-08 13:14:32 +0530

    hero added

---
+++ components/Hero.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='px-6 h-screen flex items-center'>
+      <div className='mx-auto '>
+        <div className=' flex flex-col items-start lg:items-center'>
+          <h1 className='font-semibold text-[56px] leading-[66px] tracking-[-1.25px]'>
+            Hi, I&apos;m
+          </h1>
+          <h1 className='font-semibold text-[56px] leading-[66px] tracking-[-1.25px]'>
+            Sreemikil T Manoharan
+          </h1>
+        </div>
+        <p className='font-normal mt-6 lg:mt-9 text-[20px] lg:text-center leading-[33px] text-black opacity-[64%]'>
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+          sint. Velit officia consequat duis enim velit mollit. Exercitation
+          veniam consequat sunt nostrud amet.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
     <div className='text-dark-blue'>
       <Header />
-      <p>Hello world</p>
+      <Hero />
     </div>
   )
 }
